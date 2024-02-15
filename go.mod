787f44d85f2eb29e4cbda622dc0748142643e686
BPplays <andymalbp@gmail.com>
2024-02-14 17:56:25 -0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module suzuko.org/kinit_root_fix_corrupt
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+
+func chown_r (file string, username string, group string) {
+	cmd := exec.Command("chown", fmt.Sprintf("%s:%s", username, group), "-R", file)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Println("Error running chown for", file, ":", err)
+	} else {
+		log.Println("chowned", file)
+	}
+}
+
+func k_dest (user string) {
+	cmd := exec.Command("sudo", "-u", user, "kdestroy", "-A")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Println("Error running kdestroy for", user, ":", err)
+	} else {
+		log.Println("kdestroy:", user)
+	}
+}
+
+func chmod_r (file string, perms string) {
+	cmd := exec.Command("chmod", perms, "-R", file)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Println("Error running chmod for", file, ":", err)
+	} else {
+		log.Println("chmoded", file)
+	}
+}
+
+
+
+func main() {
+	bad_msg := "kinit: Failed to store credentials: Internal credentials cache error while getting initial credentials"
+	var loops int64
+
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
+
+	// file := "/etc/krb5.keytab"
+	for {
+
+		dirPath := filepath.Join("/etc/krb5")
+		keytabFile := filepath.Join("/etc/krb5.keytab")
+
+		if _, err := os.Stat(keytabFile); err == nil {
+			loops = 0
+			for {
+				if loops > 5 {
+					break
+				}
+
+				username := "root"
+
+				chown_r(dirPath, username, username)
+
+				chmod_r(dirPath, "700")
+
+				cmd := exec.Command("sudo", "-u", username, "kinit", "-k", "-t", keytabFile)
+				cmd.Stdin = os.Stdin
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+
+				out, err := cmd.CombinedOutput()
+				if string(out) == bad_msg {
+					k_dest(username)
+					loops += 1
+				} else {
+					break
+				}
+				if err != nil {
+					log.Println("Error running kinit for", username, ":", err)
+				} else {
+					log.Println("Ran kinit for", username)
+				}
+			}
+		}
+		
+		
+		time.Sleep(3 * time.Hour)
+	}
+
+}
